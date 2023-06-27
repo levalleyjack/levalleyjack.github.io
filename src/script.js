ec5f37820f08db1aba7b9aadf12cad53dd527d45
levalleyjack <levalleyjack@gmail.com>
2023-06-27 11:31:28 -0700

    the sheep commit

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function() {
-  let header = document.querySelector('header');
-  header.classList.toggle('sticky', window.scrollY > 0);
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
